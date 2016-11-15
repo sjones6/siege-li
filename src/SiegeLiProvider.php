d3693965d80cb12c283949cff442b6c2c6dcca15
Spencer <spencer@engagedc.com>
2016-11-14 19:34:56 -0500

    Initial commit

---
+++ src/SiegeLiProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SiegeLi;
+
+use Illuminate\Support\ServiceProvider;
+
+class SiegeLiProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
