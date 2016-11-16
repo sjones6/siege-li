61f0c54a79466828d6c8c3bc66799e381f337a35
Spencer <spencer@engagedc.com>
2016-11-16 14:01:21 -0500

    Add views command

--- src/Console/MakeViewsCommand.php
+++ src/Console/MakeViewsCommand.php
@@ -2,8 +2,15 @@
 
 namespace SiegeLi\Console;
 
+// Laravel
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
+
 // Siege
+use SiegeLi\Helpers\Stub;
+use SiegeLi\Helpers\File;
 use SiegeLi\Console\SiegeCommand as Command;
+
 
 class MakeViewsCommand extends Command
 {
@@ -12,14 +19,16 @@ class MakeViewsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'siege:v {resource} {view}
+                            {--a|all : Include all optional blocks}
+                            {--o|options= : Comma delimited list of stub options to include}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Create a resourceful view';
 
     /**
      * Create a new command instance.
@@ -38,6 +47,41 @@ class MakeViewsCommand extends Command
      */
     public function handle()
     {
-        //
+
+        // Get the path and contents.
+        $path = resource_path() . '/views/' . Stub::dirName($this->argument('resource')) . Stub::bladeFileName($this->argument('view'));
+        $model = Stub::get($this->argument('view'))->make($this->getOptions());
+
+        // Make the file
+        File::put($path, $model);
+
+        $this->info('View made.');
+    }
+
+
+    /**
+    * Assembles all arguments passed by user input
+    *
+    * @param void
+    *
+    * @return array | options
+    *
+    * @author Spencer Jones
+    **/
+    protected function getOptions() {
+
+
+        $flags = !empty($this->option('options')) ? explode(',', $this->option('options')) : [];
+
+        return [
+            'vars' => new Collection([
+                'model_camel' => Str::camel($this->argument('resource')),
+                'model_slug' => Str::slug($this->argument('resource')),
+            ]),
+            'flags' => new Collection($flags),
+            'all' => (empty($flags) || $this->option('all')) ? true : false,
+        ];
+
+
     }
 }
