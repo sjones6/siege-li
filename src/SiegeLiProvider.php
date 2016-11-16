8400f700909a7983c87e259fe3c2b5c38036afca
Spencer <spencer@engagedc.com>
2016-11-16 13:59:30 -0500

    Publish stubs resources

--- src/SiegeLiProvider.php
+++ src/SiegeLiProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\ServiceProvider;
 // Package
 use SiegeLi\Console\MakeModelCommand;
 use SiegeLi\Console\MakeControllerCommand;
+use SiegeLi\Console\MakeViewsCommand;
 use SiegeLi\Console\MvcCommand;
 
 class SiegeLiProvider extends ServiceProvider
@@ -21,13 +22,20 @@ class SiegeLiProvider extends ServiceProvider
     public function boot()
     {
 
+        // Load Console commands
         if ($this->app->runningInConsole()) {
             $this->commands([
                 MakeModelCommand::class,
                 MakeControllerCommand::class,
                 MvcCommand::class,
+                MakeViewsCommand::class,
             ]);
         }
+
+        // Publish Stubs
+        $this->publishes([
+            __DIR__.'/Stubs' => resource_path('stubs'),
+        ], 'public');
 
     }
 
